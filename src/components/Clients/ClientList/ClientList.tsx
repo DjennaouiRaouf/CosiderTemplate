5dc054c8f33481ae0d331e2d8b9200e42aa4bf61
DJENNAOUI <djennaoui.raouf@gmail.com>
2023-11-19 15:32:26 +0100

    Initial commit

--- src/components/Clients/ClientList/ClientList.tsx
+++ src/components/Clients/ClientList/ClientList.tsx
@@ -19,7 +19,6 @@ interface Client {
 
 const ClientList: React.FC<any> = () => {
   const [clients, setClients] = useState<Client[]>([]);
-  const [countByCC, setCountByCC] = useState<Record<string, number>>({});
   const toast = useRef<PRToast>(null);
   const getClients = async() => {
     await axios.get(`${process.env.REACT_APP_API_BASE_URL}/sm/getclients/`,{
@@ -40,19 +39,6 @@ const ClientList: React.FC<any> = () => {
   useEffect(() => {
     getClients()
   },[clients]);
-  useEffect(() => {
-
-        const counts: Record<string, number> = {};
-        clients.forEach((obj) => {
-            const cc:any = obj.est_client_cosider;
-            if (!counts[cc]) {
-                counts[cc] = 1;
-            } else {
-                counts[cc]++;
-            }
-        });
-        setCountByCC(counts);
-        }, [clients]);
 
 
   const cosiderClientBodyTemplate= (rowData: any) => {
@@ -66,43 +52,6 @@ const ClientList: React.FC<any> = () => {
               <h3 className="text-dark mb-4">Clients</h3>
               <div className="card shadow">
                   <div className="card-body">
-                      <div className="row d-xl-flex justify-content-xl-center">
-                          {Object.keys(countByCC).map((c) => (
-                              <div  key={c}className="col-md-6 col-xl-3 mb-4">
-                                  <div className="card shadow border-start-primary py-2">
-                                      <div className="card-body">
-                                          <div className="row align-items-center no-gutters">
-                                              <div className="col me-2">
-                                                  <div className="text-uppercase text-primary fw-bold text-xs mb-1">
-                                                      {
-                                                          c === "true" ?
-                                                              <span> client interne</span>
-                                                              :
-                                                              <span> client externe </span>
-                                                      }
-
-                                                  </div>
-                                                  <div className="text-dark fw-bold h5 mb-0">
-                                                      <span>{countByCC[c]}</span>
-                                                  </div>
-                                              </div>
-                                              <div className="col-auto">
-                                                  <i className="fas fa-user fa-2x text-gray-300" />
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-
-
-
-
-                          ))}
-
-
-
-
-                      </div>
                       <div id="dataTable" className="table-responsive table mt-2" role="grid"
                            aria-describedby="dataTable_info" style={{height:"800px"}}>
                           <DataTable value={clients}  sortMode="multiple"  columnResizeMode="expand" resizableColumns paginator rows={20} rowsPerPageOptions={[20, 40, 60, 80,100]} tableStyle={{ minWidth: '50rem' }} >
